e94deb17a6c3d71367047eee6484966f20cbd23c
oluwatomisin James <jamesoluwatomisinomotosho@gmail.com>
2020-08-13 06:43:39 +0400

    playing different sounds

--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,39 @@ import 'package:flutter/material.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+
+  void playSound(int soundnum){
+    final player = AudioCache();
+    player.play('note$soundnum.wav');
+  }
+
+  Widget buildKey({int soundnum, Color color}){
+    return Expanded(
+      child: FlatButton(
+        onPressed: (){
+          playSound(soundnum);
+        },
+        color: color,
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Center(
-            child: FlatButton(
-              onPressed: (){
-                final player = AudioCache();
-                player.play('note1.wav');
-              },
-              child: Text("Click Me"),
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildKey(soundnum:1,color: Colors.blue),
+              buildKey(soundnum:2, color: Colors.red),
+              buildKey(soundnum:3, color: Colors.yellow),
+              buildKey(soundnum:4, color: Colors.green),
+              buildKey(soundnum:5, color: Colors.orange),
+              buildKey(soundnum:6, color: Colors.purple),
+              buildKey(soundnum:7, color: Colors.pink),
+            ],
           ),
         ),
       ),
